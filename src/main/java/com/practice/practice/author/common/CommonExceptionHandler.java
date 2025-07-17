5fd8fd03e4f534f3cb684d1b9734348b90688b71
Jihyeon0804 <cam080421@naver.com>
2025-07-17 12:46:35 +0900

    Common Exception Handler

---
+++ src/main/java/com/practice/practice/author/common/CommonExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.practice.practice.author.common;
+
+import com.practice.practice.author.dto.CommonErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegalException (IllegalArgumentException e) {
+        return new ResponseEntity<>(new CommonErrorDTO(HttpStatus.BAD_REQUEST.value(), e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> noSuchElementException (NoSuchElementException e) {
+        return new ResponseEntity<>(new CommonErrorDTO(HttpStatus.BAD_REQUEST.value(), e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
