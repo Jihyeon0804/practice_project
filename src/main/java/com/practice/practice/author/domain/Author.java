2bf2a1402414cd59f95d79b76da494a172dcb546
Jihyeon0804 <cam080421@naver.com>
2025-07-18 09:03:01 +0900

    Author

--- src/main/java/com/practice/practice/author/domain/Author.java
+++ src/main/java/com/practice/practice/author/domain/Author.java
@@ -1,11 +1,9 @@
 package com.practice.practice.author.domain;
 
 import com.practice.practice.author.dto.AuthorDetailDTO;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +11,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+// @Builder를 통해 유연하게 객체 생성 가능 (@AllArgsConstructor 반드시 있어야 함)
+@Builder
 @Entity
 @Getter
 @NoArgsConstructor
@@ -29,17 +29,23 @@ public class Author {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)        // db의 문자열 형태로 저장
+    @Builder.Default                    // 빌더 패턴에서 변수 초기화(디폴트 값) 시 @Builder.Default 필수
+    private Role role = Role.USER;      // 기본값을 user로 설정, 값이 할당되면 해당 값으로 세팅
+
     @CreationTimestamp
     private LocalDateTime createdTime;
 
     @UpdateTimestamp
     private LocalDateTime updatedTime;
 
-    public Author (String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+//    public Author (String name, String email, String password) {
+//        this.name = name;
+//        this.email = email;
+//        this.password = password;
+//    }
+
+
 
     public void updatePassword(String password) {
         this.password = password;
